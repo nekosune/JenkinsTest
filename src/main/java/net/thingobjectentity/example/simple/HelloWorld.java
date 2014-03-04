51b84e0a80b1d5676331aeecd8d5c66958283756
nekosune <kat.swales@nekokittygames.com>
2014-03-04 17:14:06 +0000

    First commit

---
+++ src/main/java/net/thingobjectentity/example/simple/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.thingobjectentity.example.simple;
+
+/**
+ * Created by Katrina on 04/03/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+    public static boolean ITrue()
+    {
+        return false;
+    }
+}
---
+++ src/test/java/TestTests.java
@@ -0,0 +1,17 @@
+import net.thingobjectentity.example.simple.HelloWorld;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Katrina on 04/03/14.
+ */
+public class TestTests {
+
+    @Test
+    public void test()
+    {
+        assertTrue(HelloWorld.ITrue());
+    }
+}
